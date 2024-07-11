ddf9ffa0dac180ae657eef343779a6ab7734ef95
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-07-11 19:12:56 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int add(const char* input) {
-    if (input == NULL || strlen(input) == 0) {
-        return 0;
-    }
+int isDelimiter(char c) {
+    return (c == ',' || c == '\n' || c == ';');
+}
+int isValidNumber(const char* token) {
+    int num = atoi(token);
+    return (num <= 1000);
+}
+int calculateSum(const char* input) {
     int sum = 0;
     char* token = strtok((char*)input, ",\n;");
-
     while (token != NULL) {
-        int num = atoi(token);
-        if (num <= 1000) {
-            sum += num;
+        if (isValidNumber(token)) {
+            sum += atoi(token);
         }
         token = strtok(NULL, ",\n;");
     }
     return sum;
+}
+int add(const char* input) {
+    if (input == NULL || strlen(input) == 0) {
+        return 0;
+    }
+    return calculateSum(input);
 }
